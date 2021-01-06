9c5a762053f81ea54b73139c7a8f893f692a7bc7
임동균 <70128434+Dong-Es@users.noreply.github.com>
2021-01-06 19:03:32 +0900

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("안녕하세요 오늘부터 자바 공부를 시작하였습니다. \nHello, I started studying Java from today.");
+	/** c언어 와는 다르게 prinln만 적는것이 아닌 System.out.println을 사용한다. **/
+    }
+}
