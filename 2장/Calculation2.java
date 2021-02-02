9a1861c89cafb7a89808d134dca4de9b5f82e695
임동균 <70128434+Dong-Es@users.noreply.github.com>
2021-02-03 00:57:15 +0900

    Create Calculation2.java

---
+++ 2장/Calculation2.java
@@ -0,0 +1,14 @@
+public class Calculation2 {
+    public static void main(String[] args) {
+        /*실수형으로 만든 */
+        float A=100.0f;
+        float B=2.5f;
+
+        System.out.println(A+B); /*덧셈*/
+        System.out.println(A-B); /*뺄셈*/
+        System.out.println(A*B); /*곱셈*/
+        System.out.println(A/B); /*나눗셈*/
+        System.out.println(A%B); /*나머지*/
+
+    }
+}
