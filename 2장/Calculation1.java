351c8641cf107b5f2ad0d94160ed3142c1f75ed3
임동균 <70128434+Dong-Es@users.noreply.github.com>
2021-02-03 00:50:13 +0900

    Create Calculation1.java

---
+++ 2장/Calculation1.java
@@ -0,0 +1,14 @@
+/*정수형으로 만든 사칙연산*/
+
+public class Calculation1 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+
+        System.out.println(a+b); /*덧셈*/
+        System.out.println(a-b); /*뺄셈*/
+        System.out.println(a*b); /*곱셈*/
+        System.out.println(a/b); /*나눗셈*/
+        System.out.println(a%b); /*나머지*/
+    }
+}
