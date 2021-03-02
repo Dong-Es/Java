3f3dc69396564ec2383d262f8acc17a4228d7da9
임동균 <70128434+Dong-Es@users.noreply.github.com>
2021-03-02 22:16:32 +0900

    Create fortest3.java

---
+++ 4장/fortest3.java
@@ -0,0 +1,9 @@
+public class fortest3 {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i= 0; i<=10; i++){
+            sum = sum+ i;
+            System.out.println("0부터 10까지 합은"+sum);
+        }
+    }
+}
